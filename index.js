56d7832b7f1c43d1c926f08960c3468a1660a086
wooooodfire <jeffrey98064@gmail.com>
2022-05-25 19:08:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+const { readdirSync } = require('fs');
+
+mongoose
+	.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('MONGO CONNECTION OPEN!!!');
+	})
+	.catch((err) => {
+		console.log('OH NO MONGO CONNECTION ERROR!!!!');
+		console.log(err);
+	});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}))
+
+app.get('/products', async (req, res) => {
+	const products = await Product.find({});
+	res.render('products/index', { products });
+});
+
+app.get('/products/new', (req, res)=>{
+    res.render('products/new')
+})
+
+
+app.post('/products', async(req, res)=>{
+    const newProduct = new Product(req.body);
+    await newProduct.sa;
+    console.log(newProduct);
+    res.send('make your own product')
+})
+
+app.get('/products/:id', async(req, res)=>{
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.render('products/show', {product})
+})
+
+
+
+app.listen(3000, () => {
+	console.log('APP IS LISTENING ON PORT 3000');
+});
